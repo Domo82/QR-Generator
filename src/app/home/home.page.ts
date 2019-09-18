33de36ae860932cd947d5e674ede54ebd572bed3
Domo82 <domodillon82@gmail.com>
2019-09-18 20:30:05 +0100

    button to DL QR image

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,7 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  [x: string]: any;
   qrData = 'https://ionicacademy.com/';
   scannedCode = null;
   elementType: 'url' | 'canvas' | 'img' = 'canvas';
@@ -25,6 +26,24 @@ export class HomePage {
           this.scannedCode = barcodeData.text;
         },
         err => console.log('Scan Error: ', err)
+      );
+    }
+
+    downloadQR() {
+      const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+      const imageData = canvas.toDataURL('image/jpeg').toString();
+
+      var data = imageData.split(',')[1];
+
+      this.base64ToGallery.base64ToGallery(data, { prefix: '_img', mediaScanner: true })
+      .then(
+        async res => {
+          let toast = await this.toatCtrl.create({
+            header: 'QR Code saved in your photo library'
+          });
+          toast.present();
+        },
+        err => console.log('Error saving image to gallery', err)
       );
     }
 }
