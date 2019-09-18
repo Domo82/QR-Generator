6591d44e7098f23b56cddad3fecfc5f8861c2cee
Domo82 <domodillon82@gmail.com>
2019-09-18 19:57:25 +0100

    start of qr code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  qrData = 'https://ionicacademy.com/';
+  scannedCode = null;
+  elementType: 'url' | 'canvas' | 'img' = 'canvas';
 
-  constructor() {}
+  constructor(
+    private barcodescanner: BarcodeScanner,
+    private base64: Base64ToGallery,
+    private toatCtrl: ToastController
+    ) {}
 
+    scanCode() {
+      this.barcodescanner.scan().then(
+        barcodeData => {
+          this.scannedCode = barcodeData.text;
+        },
+        err => console.log('Scan Error: ', err)
+      );
+    }
 }
